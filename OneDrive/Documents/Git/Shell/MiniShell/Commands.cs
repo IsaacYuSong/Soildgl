247be70287e6baeec8085b41b28d42df9dabb96e
Ice <isaac.song12345.r.r@gmail.com>
2023-10-28 18:51:21 -0700

    mini shell will add more commands soon

---
+++ OneDrive/Documents/Git/Shell/MiniShell/Commands.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Channels;
+using System.Xml;
+
+namespace MiniShell
+{
+    public class Commands
+    {
+        
+
+        public static void Cd(string? newDirectoryPath = null)
+        {
+            
+            if (string.IsNullOrEmpty(newDirectoryPath)|| newDirectoryPath.Trim() == "")
+            {
+                newDirectoryPath = Directory.GetCurrentDirectory();
+                
+            }
+            DirectoryInfo directoryInfo = new DirectoryInfo(newDirectoryPath);
+            if (directoryInfo.Exists)
+            {
+                Directory.SetCurrentDirectory(newDirectoryPath);
+                
+            }
+            else
+            {
+                Console.WriteLine("Directory " + newDirectoryPath + " does not exist.");
+            }
+        }
+        public static void Ls(string currentDirectory=null)
+        {
+            
+        }
+    }
+}
---
+++ OneDrive/Documents/Git/Shell/MiniShell/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace MiniShell
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write(">");
+                string input = Console.ReadLine();
+                string[] words = input.Split(" ");
+                switch(words[0])
+                {
+                    
+                    case "cd":
+                        if (words.Length > 1) 
+                        {
+                            Commands.Cd(words[1]);
+                        }
+                        else
+                        {
+                            Commands.Cd();
+                        }
+                        break;
+                    case "ls":
+                        if(words.Length > 1)
+                        {
+                            Commands.Ls(words[1]);
+                        }
+                        else
+                        {
+                            Commands.Ls();
+                        }
+                        
+                        break;
+                    case "clear":
+                        Console.Clear();
+                        break;
+                   
+                }
+            }
+
+        }
+
+    }
+}
